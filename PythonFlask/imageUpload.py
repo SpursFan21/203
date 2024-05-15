cf0256907fde233e14eb9f4df0d5ec1711656ef5
SpursFan21 <duncanfrederic@hmail.com>
2024-05-16 11:34:49 +1200

    image uploader still not woeking - renamed PythonFlask

--- PythonFlask/imageUpload.py
+++ PythonFlask/imageUpload.py
@@ -1,11 +1,14 @@
-from flask import Flask, render_template, request, send_from_directory
+from flask import Flask, render_template, request, send_from_directory, redirect, url_for
 from werkzeug.utils import secure_filename
 import os
 
 app = Flask(__name__)
 
-UPLOAD_FOLDER = 'Image'
+UPLOAD_FOLDER = 'PythonFlask/static/Image'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
 
 @app.route("/")
 def upload():
@@ -17,11 +20,15 @@ def uploader():
         f = request.files["file"]
         filename = secure_filename(f.filename)
         f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        return "File uploaded successfully"
+        return redirect(url_for('uploaded_file', filename=filename))
 
-@app.route('/Image/<filename>')
+@app.route('/PythonFlask/static/Image/<filename>')
 def uploaded_file(filename):
+    file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    print("File Path:", file_path)  # Print out the file path for debugging
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
